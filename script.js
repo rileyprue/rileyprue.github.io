005f5d6dc5a8c23109ed34dd0e9fe9dae52bc344
rileyprue <riley@prue.cc>
2026-07-28 20:55:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.15 });
+
+document.querySelectorAll('.reveal').forEach(el => {
+  observer.observe(el);
+});
