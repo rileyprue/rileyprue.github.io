61e1ceae728587bc8fdb5669f832ef595afbbd52
rileyprue <riley@prue.cc>
2026-07-31 10:00:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+// Theme toggle
 const toggle = document.querySelector('.theme-toggle');
 
+// Dark mode is default
+document.body.classList.remove('light-mode');
+
+// Show sun because clicking it will enable light mode
+toggle.innerHTML = '☀️';
+
 toggle.addEventListener('click', () => {
+
   document.body.classList.toggle('light-mode');
+
+  // Update icon
+  if (document.body.classList.contains('light-mode')) {
+
+    // Light mode is active, show moon to go back dark
+    toggle.innerHTML = '🌙';
+
+  } else {
+
+    // Dark mode is active, show sun to go light
+    toggle.innerHTML = '☀️';
+
+  }
+
 });
